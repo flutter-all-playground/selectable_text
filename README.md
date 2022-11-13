3c149a6653e79da25e57dfeb699faf2116cde049
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-11-13 08:31:26 -0300

    Add Selectable Text sample - Only to get Text

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # selectable_text
+
+<div align='center'>
+  <img src='https://github.com/phferreira/assets/blob/master/images/selectable_text.png'/>
+</div>
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Selectable Text',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 74),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Selectable Text'),
+      ),
+      body: const Center(
+        child: SelectableText(
+          'Selectable Text',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
